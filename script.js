0d113c050343f25caed3337200e3fddb3a35e7d7
keyoletoltu4-glitch <233703984+keyoletoltu4-glitch@users.noreply.github.com>
2026-09-11 23:25:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = "https://mufssprelgsroumvmrfk.supabase.co";
 const supabaseKey = "sb_publishable_aTOd54VsmDOAJVBssxtrug_nV1_Vhas";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
 
 const seedRequests = [
   {
